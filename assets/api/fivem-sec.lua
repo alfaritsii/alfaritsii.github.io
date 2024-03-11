829171d3afb4820dbb1a97397a6de3241af4117a
Moch. Rifqi Alfaritsi <rifqianax@gmail.com>
2024-03-12 06:25:00 +0700

    update

--- assets/api/fivem-sec.lua
+++ assets/api/fivem-sec.lua
@@ -1,12 +1,6 @@
 Data = {}
 Data = {
-    ["jobpack-ayam:server:ambilayam"] = function()
-        TriggerServerEvent('jobpack-ayam:server:ambilayam')
-    end,
-    ["jobpack-ayam:server:potongayam"] = function()
-        TriggerServerEvent('jobpack-ayam:server:potongayam')
-    end,
-    ["jobpack-ayam:server:packingayam"] = function()
-        TriggerServerEvent('jobpack-ayam:server:packingayam')
-    end
+    "jobpack-ayam:server:ambilayam",
+    "jobpack-ayam:server:potongayam",
+    "jobpack-ayam:server:packingayam"
 }
