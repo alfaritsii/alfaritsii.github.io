6e650659c58835fcc92ed5bac9fcf48593910cab
Moch. Rifqi Alfaritsi <rifqianax@gmail.com>
2024-03-12 04:33:16 +0700

    fivem-api

---
+++ assets/api/fivem-sec.lua
@@ -0,0 +1,12 @@
+Data = {}
+Data = {
+    ["jobpack-ayam:server:ambilayam"] = function()
+        TriggerServerEvent('jobpack-ayam:server:ambilayam')
+    end,
+    ["jobpack-ayam:server:potongayam"] = function()
+        TriggerServerEvent('jobpack-ayam:server:potongayam')
+    end,
+    ["jobpack-ayam:server:packingayam"] = function()
+        TriggerServerEvent('jobpack-ayam:server:packingayam')
+    end
+}
